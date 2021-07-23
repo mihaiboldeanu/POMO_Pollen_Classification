448c1f224bd7b0d349875d8fbe73abb7ef1e9fc3
mihaiboldeanu <35964395+mihaiboldeanu@users.noreply.github.com>
2021-07-23 15:22:43 +0300

    Update conv_net_v4.py

--- pomo_cnn/scripts/conv_net_v4.py
+++ pomo_cnn/scripts/conv_net_v4.py
@@ -9,14 +9,19 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential,load_model,Model
-from tensorflow.keras.layers import Conv2D,Dropout,Dense,MaxPooling2D,Activation
-from tensorflow.keras.layers import Input,Flatten,BatchNormalization,Add
+from tensorflow.keras.layers import Conv2D,Dropout,Dense,MaxPooling2D,Activation,SeparableConv2D 
+from tensorflow.keras.layers import Input,Flatten,BatchNormalization,Add,GlobalAveragePooling2D,LeakyReLU
+from tensorflow.keras import regularizers
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam,SGD
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
 import tensorflow as tf
+import json 
+import lib_pomo
+from tensorflow.keras.applications import ResNet50,Xception
+tf.keras.backend.clear_session() 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 def read_pictures(path):
     image_array = plt.imread(path)[:,:,0]
@@ -28,139 +33,133 @@ def div_to_float(img):
     image=np.array(img)
     return image/255.
 
-datagen_train = ImageDataGenerator(fill_mode='constant',cval=0,
-                             rotation_range=90,
-                             #width_shift_range=0.1,
-                             #height_shift_range=0.1,
-                             vertical_flip=True,
-                             horizontal_flip=True,
-                             #channel_shift_range=0.1,
-                             preprocessing_function = div_to_float)
+datagen_train = ImageDataGenerator( rescale=1/255.,fill_mode='constant',cval=0,
+                                    rotation_range=360,
+                                    channel_shift_range=32,
+                                    zoom_range = 0.1,
+                                    shear_range = 0.2,
+                                    vertical_flip=True,
+                                    horizontal_flip=True)
 
-datagen_val_test = ImageDataGenerator(preprocessing_function = div_to_float)
+datagen_val_test = ImageDataGenerator(rescale=1/255.,)
 # load and iterate training dataset
-train_it = datagen_train.flow_from_directory(r'C:\Users\Mihai Boldeanu\Desktop\pollen_clas\data\train\\',
+train_it_aug = datagen_train.flow_from_directory(r'C:\Users\Mihai Boldeanu\Desktop\pollen_clas\data\train\\',
                                              color_mode="grayscale",
                                              shuffle=True,
                                              class_mode='categorical',
                                              target_size=(360, 360) ,
-                                             batch_size=16)
-# load and iterate validation dataset
-val_it = datagen_val_test.flow_from_directory(r'C:\Users\Mihai Boldeanu\Desktop\pollen_clas\data\validation\\',
-                                              color_mode="grayscale",
-                                              shuffle=True,
-                                              class_mode='categorical',
-                                              target_size=(360, 360),
-                                              batch_size=16)
+                                             batch_size=64,)
 # load and iterate test dataset
-test_it = datagen_val_test.flow_from_directory(r'C:\Users\Mihai Boldeanu\Desktop\pollen_clas\data\test\\',
+train_it = datagen_val_test.flow_from_directory(r'C:\Users\Mihai Boldeanu\Desktop\pollen_clas\data\train\\',
                                                color_mode="grayscale",
                                                shuffle=True,
                                                class_mode='categorical',
                                                target_size=(360, 360),
-                                               batch_size=16)
+                                               batch_size=64)
 
 
-input_image = Input(shape=(360, 360,1))
-
-x = Conv2D(64, (3, 3),strides=1,padding='same')(input_image)
-x = MaxPooling2D(pool_size=(2, 2))(x)
-X = Activation("relu")(x)
-x = Conv2D(64, (3, 3),strides=2,padding='same')(x)
-X = Activation("relu")(x)
-
-x = Conv2D(64, (3, 3),strides=1,padding='same')(x)
-x = MaxPooling2D(pool_size=(2, 2))(x)
-X = Activation("relu")(x)
-x = Conv2D(128, (3, 3),strides=2,padding='same')(x)
-X = Activation("relu")(x)
-
-x = Conv2D(128, (3, 3),strides=1,padding='same')(x)
-x = MaxPooling2D(pool_size=(2, 2))(x)
-X = Activation("relu")(x)
-x = Conv2D(128, (3, 3),strides=2,padding='same')(x)
-X = Activation("relu")(x)
-
-x = Conv2D(256, (3, 3),strides=1,padding='same')(x)
-X = Activation("relu")(x)
-x = Conv2D(512, (3, 3),strides=2,padding='same')(x)
-X = Activation("relu")(x)
-
-x = Conv2D(512, (3, 3),strides=1,padding='same')(x)
-x = Conv2D(512, (3, 3),strides=2,padding='same')(x)
-X = Activation("relu")(x)
+# load and iterate validation dataset
+val_it = datagen_val_test.flow_from_directory(r'C:\Users\Mihai Boldeanu\Desktop\pollen_clas\data\validation\\',
+                                              color_mode="grayscale",
+                                              shuffle=False,
+                                              class_mode='categorical',
+                                              target_size=(360, 360),
+                                              batch_size=64)
 
 
-x_flat = Flatten()(x)
 
-x_dense = Dense(256,activation="relu")(x_flat)
-x_dense = Dropout(0.25)(x_dense)
-x_dense = Dense(256,activation="relu")(x_dense)
-x_dense = Dropout(0.25)(x_dense)
-x_dense = Dense(128,activation="relu")(x_dense)
-x_dense = Dropout(0.25)(x_dense)
-x_out = Dense(19)(x_dense)
-x_out = Activation('softmax', dtype='float32', name='predictions')(x_out)
 
-model = Model(input_image,x_out)
-
-adam = Adam(learning_rate=0.0001, beta_1=0.7, beta_2=0.999, amsgrad=False) 
-#model = load_model("cnn_model-87-0.88.hdf5")
+model_id = 9
+print(model_id)
+model = lib_pomo.build_cnn(model_id)
+adam = Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False)
 model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['categorical_accuracy'])
-model.summary()
-# checkpoint
+# model.summary()
 
-filepath="cnn_model-{epoch:02d}-{val_categorical_accuracy:.2f}.hdf5"
-checkpoint = ModelCheckpoint(filepath,
+reduce_lr = ReduceLROnPlateau(monitor='loss',
+                              mode='min',
+                              verbose = 1,
+                              factor=0.9,
+                              patience=20,
+                              cooldown=2,
+                              min_lr=0.00000001)
+
+
+filepath="cnn_model-"+str(model_id)+"-aug-{val_categorical_accuracy:.5f}.hdf5"
+checkpoint_aug = ModelCheckpoint(filepath,
                              monitor='val_categorical_accuracy',
                              verbose=1,
                              save_best_only=True,
                              mode='max')
-reduce_lr = ReduceLROnPlateau(monitor='val_categorical_accuracy',
-                              mode='max',
-                              verbose = 1,
-                              factor=0.5,
-                              patience=10,
-                              cooldown=2,
-                              min_lr=0.0000001)
-es = EarlyStopping(monitor='val_categorical_accuracy',
-                   mode='max',
-                   verbose=1,
-                   patience=50)
-callbacks_list = [checkpoint,reduce_lr,es]
-#callbacks=callbacks_list
+
+callbacks_list_aug = [checkpoint_aug,reduce_lr]
+
+history = model.fit(train_it_aug,
+                    epochs=500,
+                    callbacks=callbacks_list_aug,
+                    verbose=1,
+                    shuffle=True,
+                    validation_data=val_it,
+                    workers=4)  
+np.save("cnn_model_"+str(model_id)+"_history_aug.npy",history.history)
+
+# model = load_model("cnn_model-6-aug-0.90904.hdf5")
+
 history = model.fit(train_it,
-                    epochs=200,
-                    callbacks=callbacks_list,
+                    epochs=100,
+                    callbacks=callbacks_list_aug,
                     verbose=1,
                     shuffle=True,
                     validation_data=val_it,
                     workers=4)
 
-plt.figure(figsize=(10,10))  
-plt.plot(history.history['categorical_accuracy'],'r')  
-plt.plot(history.history['val_categorical_accuracy'],'g')  
-#plt.xticks(np.arange(0, 11, 2.0))  
-plt.rcParams['figure.figsize'] = (8, 6)  
-plt.xlabel("Num of Epochs")  
-plt.ylabel("Accuracy")  
-plt.title("Training Accuracy vs Validation Accuracy")  
-plt.legend(['train','validation'])
+np.save("cnn_model_"+str(model_id)+"_history.npy",history.history)
+
+
+# ceva = train_it_aug.next()
+# image = ceva[0][1]
+# plt.figure()
+# plt.imshow(image,cmap="gray")
+# image = ceva[0][1]>0
+
+# plt.figure()
+# plt.imshow(image,cmap="gray")
+
+# plt.figure(figsize=(10,10))  
+# plt.plot(history.history['categorical_accuracy'],'r')  
+# plt.plot(history.history['val_categorical_accuracy'],'g')  
+# #plt.xticks(np.arange(0, 11, 2.0))  
+# plt.rcParams['figure.figsize'] = (8, 6)  
+# plt.xlabel("Num of Epochs")  
+# plt.ylabel("Accuracy")  
+# plt.title("Training Accuracy vs Validation Accuracy")  
+# plt.legend(['train','validation'])
 
 
 
-plt.figure(figsize=(10,10))  
-plt.plot(history.history['loss'],'r')  
-plt.plot(history.history['val_loss'],'g')  
-#plt.xticks(np.arange(0, 11, 2.0))  
-plt.rcParams['figure.figsize'] = (8, 6)  
-plt.xlabel("Num of Epochs")  
-plt.ylabel("Loss")  
-plt.title("Training Loss vs Validation Loss")  
-plt.legend(['train','validation'])
+# plt.figure(figsize=(10,10))  
+# plt.plot(history.history['loss'],'r')  
+# plt.plot(history.history['val_loss'],'g')  
+# #plt.xticks(np.arange(0, 11, 2.0))  
+# plt.rcParams['figure.figsize'] = (8, 6)  
+# plt.xlabel("Num of Epochs")  
+# plt.ylabel("Loss")  
+# plt.title("Training Loss vs Validation Loss")  
+# plt.legend(['train','validation'])
+# plt.show() 
 
-plt.show() 
+# history.history['categorical_accuracy'],'r')  
+# plt.plot(history.history['val_categorical_accuracy'],'g')  
+# #plt.xticks(np.arange(0, 11, 2.0))  
+# plt.rcParams['figure.figsize'] = (8, 6)  
+# plt.xlabel("Num of Epochs")  
+# plt.ylabel("Accuracy")  
+# plt.title("Training Accuracy vs Validation Accuracy")  
+# plt.legend(['train','validation'])
 
 
-print(model.predict(test_it))
-model.save("best_model_clean_set.h5")
+
+# plt.figure(figsize=(10,10))  
+# plt.plot(history.history['loss'],'r')  
+# plt.plot(history.history['val_loss'],'g')  
+
